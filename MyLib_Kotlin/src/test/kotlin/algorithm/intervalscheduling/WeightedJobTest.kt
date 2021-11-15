9b59e21958c14cdc97a70e3e9bd030884bb51eee
CWKSC <53114952+CWKSC@users.noreply.github.com>
2021-11-16 01:11:16 +0800

    Update WeightedJobTest.kt

--- MyLib_Kotlin/src/test/kotlin/algorithm/intervalscheduling/WeightedJobTest.kt
+++ MyLib_Kotlin/src/test/kotlin/algorithm/intervalscheduling/WeightedJobTest.kt
@@ -25,8 +25,33 @@ internal class WeightedJobTest {
         val size = selectedJobs.size
 
         assertAll(
-            { assertEquals(sum, 8.0) },
-            { assertEquals(size, 2) }
+            { assertEquals(8.0, sum) },
+            { assertEquals(2, size) }
+        )
+    }
+
+    @Test
+    fun test_findMaxSumWeightJobsWithoutOverlap2() {
+
+        val jobs = arrayOf(
+            WeightedJob(0.0, 0.0, 0.0),
+            WeightedJob(0.0, 5.0, 4.0),
+            WeightedJob(6.0, 9.0, 5.0),
+            WeightedJob(5.0, 8.0, 6.0),
+            WeightedJob(3.0, 6.0, 5.0),
+            WeightedJob(6.0, 7.0, 4.0),
+            WeightedJob(8.0, 11.0, 12.0),
+            WeightedJob(7.0, 10.0, 2.0),
+            WeightedJob(9.0, 13.0, 7.0),
+        )
+
+        val selectedJobs = jobs.findMaxSumWeightJobsWithoutOverlap()
+        val sum = selectedJobs.sumOf { it.weight }
+        val size = selectedJobs.size
+
+        assertAll(
+            { assertEquals(22.0, sum) },
+            { assertEquals(3, size) }
         )
     }
 
